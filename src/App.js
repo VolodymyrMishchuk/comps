8a6f53beb27647566fbc8f26f613a7c628feb3f5
Volodymyr Mishchuk <vereteno.school@gmail.com>
2023-09-29 21:59:58 +0200

    Worked with librarys classnames and react icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import Button from './Button';
+import { GoBell, GoCloud, GoDatabase } from 'react-icons/go';
 
 function App() {
     return (
-    <div>
         <div>
-            <Button success >Click me!</Button>
-        </div>        
-        <div>
-            <Button>Buy now!</Button>
-        </div>        
-        <div>
-            <Button>See Deal!</Button>
-        </div>        
-        <div>
-            <Button>Hide Ads!</Button>
-        </div>        
-        <div>
-            <Button>Something!</Button>
+            <div>
+                <Button 
+                    success 
+                    rounded 
+                    outline
+                    className="mb-5"
+                >
+                    <GoBell />
+                    Click me!
+                </Button>
+            </div>        
+            <div>
+                <Button 
+                    danger 
+                    outline
+                >
+                    <GoCloud />
+                    Buy now!
+                </Button>
+            </div>        
+            <div>
+                <Button 
+                    warning
+                >
+                    <GoDatabase />
+                    See Deal!
+                </Button>
+            </div>        
+            <div>
+                <Button 
+                    secondary 
+                    outline
+                >
+                    Hide Ads!
+                </Button>
+            </div>        
+            <div>
+                <Button 
+                    primary 
+                    rounded
+                >
+                    Something!
+                </Button>
+            </div>
         </div>
-    </div>
     );
 }
 
